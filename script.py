ef595df54c353a9eb460755ee9b27504fe5005ae
ximenabeyliss01 <72842439+ximenabeyliss01@users.noreply.github.com>
2020-10-15 22:35:43 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,20 @@
-ola
+from Bio import SeqIO
+from Bio.SeqFeature import FeatureLocation, SeqFeature
+from Bio.SeqRecord import SeqRecord
+import os
+filename= "/mnt/c/Users/usuario/Desktop/ls_orchid.gbk"
+def summarize_contents(filename):
+	all_records=[]
+	records = list(SeqIO.parse(filename, "genbank"))
+	print ("Path: ", os.path.dirname(filename))
+	print("num_records = %i records" % len(records))
+	print("\n\n")
+	
+	for seq_record in SeqIO.parse(filename, "genbank"):
+		all_records.append(seq_record.name)
+		print("Name: ", seq_record.name)
+		print("ID:",seq_record.id)
+		print("\n")
+		
+		
+summarize_contents(filename)
