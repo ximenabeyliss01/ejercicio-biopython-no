024dcdacc326d21acd0e440448cf45bc7cafcfb3
ximenabeyliss01 <72842439+ximenabeyliss01@users.noreply.github.com>
2020-10-27 17:00:37 -0600

    Update script.py

--- script.py
+++ script.py
@@ -3,18 +3,37 @@ from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
 import os
 filename= "/mnt/c/Users/usuario/Desktop/ls_orchid.gbk"
+#Creacion de funcion
 def summarize_contents(filename):
-	all_records=[]
-	records = list(SeqIO.parse(filename, "genbank"))
-	print ("Path: ", os.path.dirname(filename))
-	print("num_records = %i records" % len(records))
-	print("\n\n")
+	File_List = []
+	File_Extension = []
 	
-	for seq_record in SeqIO.parse(filename, "genbank"):
-		all_records.append(seq_record.name)
-		print("Name: ", seq_record.name)
-		print("ID:",seq_record.id)
-		print("\n")
+	File_List = os.path.split(filename)
+	File_   Extension = os.path.split(filename)
+	
+	if(File_Extension[1] == ".gbk"):
+		type_file= "genbank"
+	else:
+		type_file= "fasta"
 		
+	record = list(SeqIO.parse(filename, type_file))
+	
+	d={}
+	d['File:'] = File_List[1]
+	d['Path:'] = File_List[0]
+	d['Num_records:'] = len(record)
+	d['Names:'] = []
+	d['IDs:'] = []
+	d['Descriptions'] = []
+	
+	
+	for seq_record in SeqIO.parse(filename, type_file):
+		d['Names:'].append(seq_rcd.name)
+		d['IDs:'].append(seq_rcd.id)
+		d['Descriptions'].append(seq_rcd.description)
+	return d
+
+if __name__ == "__main__":
+	resultado = summarize_contents(filename)
 		
 summarize_contents(filename)
